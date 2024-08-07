031bea5a651815ec7084cb19ccbd751a0ec2fc0a
GeekyNaved <navedahmed040@gmail.com>
2024-08-07 14:18:35 +0530

    topNavbar-> adding circular bg in links & fixing mobile issues

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,4 +1,4 @@
-import {AiOutlineUser} from "react-icons/ai";
+import {AiOutlineSetting, AiOutlineUser} from "react-icons/ai";
 import {CiMail} from "react-icons/ci";
 import {IoIosNotificationsOutline} from "react-icons/io";
 import {NavLink} from "react-router-dom";
@@ -9,22 +9,28 @@ const TopNavbar = () => {
     <div className="bg-slate-900 text-slate-50">
       <div className="flex justify-between items-center h-20 px-2 md:px-4">
         <SearchInputField />
-        <div className="flex gap-6 cursor-pointer">
+        <div className="flex gap-2 md:gap-6 cursor-pointer">
           <NavLink
-            to="/search"
-            className="md:text-2xl underline underline-offset-4 decoration-2 decoration-slate-50"
+            to="/settings"
+            className="md:text-2xl underline underline-offset-4 decoration-2 decoration-slate-50 p-2 rounded-full bg-slate-600"
           >
             <CiMail className="hover:text-blue-300" />
           </NavLink>
           <NavLink
-            to="/cart"
-            className="md:text-2xl  underline underline-offset-4 decoration-2 decoration-slate-50"
+            to="/settings"
+            className="md:text-2xl  underline underline-offset-4 decoration-2 decoration-slate-50 p-2 rounded-full bg-slate-600"
           >
             <IoIosNotificationsOutline className="hover:text-blue-300" />
           </NavLink>
           <NavLink
-            to="/account"
-            className="md:text-2xl  underline underline-offset-4 decoration-2 decoration-slate-50"
+            to="/settings"
+            className="md:text-2xl  underline underline-offset-4 decoration-2 decoration-slate-50 p-2 rounded-full bg-slate-600"
+          >
+            <AiOutlineSetting className="hover:text-blue-300" />
+          </NavLink>
+          <NavLink
+            to="/settings"
+            className="md:text-2xl  underline underline-offset-4 decoration-2 decoration-slate-50 p-2 rounded-full bg-slate-600"
           >
             <AiOutlineUser className="hover:text-blue-300" />
           </NavLink>
